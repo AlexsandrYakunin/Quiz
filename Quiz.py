98231c5826832643af4f5c90ea49636a44b6a72a
AlexsandrYakunin <98354125+AlexsandrYakunin@users.noreply.github.com>
2023-03-02 14:22:51 +0300

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.title("Викторина")
+root.geometry("400x400")
+
+def que_one():
+    question = Label(root, text="Висит груша, нельзя скушать?")
+    answer = Entry()
+    btn = Button(root, text="Ответить!", command=lambda: game1(que_two))
+    question.grid()
+    answer.grid()
+    btn.grid()
+
+    def game1(que_two):
+        if answer.get().lower() == "лампочка":
+            que_two()
+        else:
+            messagebox.showerror("Ошибка!", "Попробуй ещё раз!")
+
+def que_two():
+    question_2 = Label(root, text="Зимой и летом одним цветом?")
+    answer_2 = Entry()
+    btn_2 = Button(root, text="Ответить!", command=lambda: game2(que_two))
+    question_2.grid()
+    answer_2.grid()
+    btn_2.grid()
+    
+    def game2(que_two):
+        if answer_2.get().lower() == "ёлка":
+            messagebox.showinfo("Победа!", "Ты молодец!")
+        elif answer_2.get().lower() == "елка":
+            messagebox.showinfo("Победа!", "Ты молодец!")
+        else: 
+            messagebox.showerror("Ошибка!", "Попробуй ещё раз!")
+
+que_one()
+
+root.mainloop()
